12e8e3003f45b676bd160596d99efc9c639feb67
Ben New <ben@leftclick.com.au>
2012-07-26 11:24:35 +0000

    Add initial version.

---
+++ demo/index.php
@@ -0,0 +1,202 @@
+<!doctype html>
+<html>
+<head>
+	<title>Test page for jquery-clock plugin</title>
+	<style type="text/css">
+		.clock { display: inline-block; font-size: x-large; font-family: monospace; white-space: nowrap; }
+		#clock2.clock { font-size: 1em; }
+		.clock .date-part { color: red; }
+		.clock .time-part { color: blue; }
+		pre { margin: 0; }
+		table#demonstrations { border-collapse: collapse; }
+		table#demonstrations th, table#demonstrations td { vertical-align: top; border: 1px #666 solid; padding: 1em; }
+		table#demonstrations th { background-color: #eee; }
+		.popup { position: absolute; z-index: 100; background-color: #eee; border: 1px solid #000; padding: .5em; min-width: 300px; }
+		.display-hover { cursor: pointer; }
+	</style>
+</head>
+<body>
+<h1>jQuery Clock plugin test page</h1>
+<p>
+The below timestamp is printed by PHP, and then updated by jQuery.
+The timestamp can be printed as an integer representing milliseconds since epoch (i.e. <code>&lt;?php echo time(); ?&gt;</code>), or in any format that can be interpreted by <code>Date.parse()</code>.
+</p>
+<p>
+If PHP doesn't print the timestamp, it will use the current local time from the browser.
+</p>
+<p>
+If enabled, periodic calls are made to the PHP using AJAX, to ensure the timer is kept synchronised with the server.
+</p>
+
+<table id="demonstrations">
+	<thead>
+	<tr>
+		<th class="demonstration">Demonstration</th>
+		<th class="summary">Summary</th>
+		<th class="explanation">Initial Content</th>
+		<th class="explanation">AJAX Requests</th>
+		<th class="explanation">Additional Settings</th>
+		<th class="javascript">JavaScript</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<td class="demonstration"><div id="clock0" class="clock"></div></td>
+		<td class="summary">Default behaviour, no server time</td>
+		<td class="explanation">Empty (use browser time)</td>
+		<td class="explanation">None</td>
+		<td class="explanation">None</td>
+		<td class="javascript">
+<pre>
+<?php ob_start(); ?>
+$(function() {
+	$('#clock0').clock();
+});
+<?php echo htmlentities(ob_get_clean()); ?>
+</pre>
+		</td>
+	</tr>
+	<tr>
+		<td class="demonstration"><div id="clock1" class="clock"><?php echo time(); ?></div></td>
+		<td class="summary">Default behaviour with initial server time</td>
+		<td class="explanation">Timestamp from server:<br/><code><?php echo time(); ?></code></td>
+		<td class="explanation">None</td>
+		<td class="explanation">None</td>
+		<td class="javascript">
+<pre>
+<?php ob_start(); ?>
+$(function() {
+	$('#clock1').clock();
+});
+<?php echo htmlentities(ob_get_clean()); ?>
+</pre>
+		</td>
+	</tr>
+	<tr>
+		<td class="demonstration"><div id="clock2" class="clock"><?php echo date('D, d M Y H:i:s'); ?></div></td>
+		<td class="summary">Custom formatting with initial server time</td>
+		<td class="explanation">Formatted date from server:<br/><code><?php echo date('D, d M Y H:i:s'); ?></code></td>
+		<td class="explanation">None</td>
+		<td class="explanation"><code>outputFormat</code> demonstrating a different style of output</td>
+		<td class="javascript">
+<pre>
+<?php ob_start(); ?>
+$(function() {
+	$('#clock2').clock({
+		outputFormat: '%i minutes and %s seconds past %g%a on %l the %d%S of %F in the year %Y'
+	});
+});
+	<?php echo htmlentities(ob_get_clean()); ?>
+</pre>
+		</td>
+	</tr>
+	<tr>
+		<td class="demonstration"><div id="clock3" class="clock"><?php echo time(); ?></div></td>
+		<td class="summary">Custom formatting (HTML) with initial server time</td>
+		<td class="explanation">Timestamp from server:<br/><code><?php echo time(); ?></code></td>
+		<td class="explanation">None</td>
+		<td class="explanation"><code>outputFormat</code> demonstrating use of HTML markup in the output</td>
+		<td class="javascript">
+<pre>
+<?php ob_start(); ?>
+$(function() {
+	$('#clock3').clock({
+		outputFormat: '<span class="date-part">%d-%m-%Y</span> <span class="time-part">%H:%i:%s</span>'
+	});
+});
+<?php echo htmlentities(ob_get_clean()); ?>
+</pre>
+		</td>
+	</tr>
+	<tr>
+		<td class="demonstration"><div id="clock4" class="clock"><?php echo date('Y/m/d H:i:s'); ?></div></td>
+		<td class="summary">Default behaviour with initial formatted server time</td>
+		<td class="explanation">Formatted date from server:<br/><code><?php echo date('Y/m/d H:i:s'); ?></code></td>
+		<td class="explanation">None</td>
+		<td class="explanation">None</td>
+		<td class="javascript">
+<pre>
+<?php ob_start(); ?>
+$(function() {
+	$('#clock4').clock();
+});
+<?php echo htmlentities(ob_get_clean()); ?>
+</pre>
+		</td>
+	</tr>
+	<tr>
+		<td class="demonstration"><div id="clock5" class="clock"><?php echo time(); ?></div></td>
+		<td class="summary">Default behaviour with initial server timestamp and AJAX updates (also timestamps)</td>
+		<td class="explanation">Timestamp from server:<br/><code><?php echo time(); ?></code></td>
+		<td class="explanation">Once per minute from <code>date.php</code> (returns a timestamp from server)</td>
+		<td class="explanation">None</td>
+		<td class="javascript">
+<pre>
+<?php ob_start(); ?>
+$(function() {
+	$('#clock5').clock({
+		ajaxInterval: 60000,
+		ajaxUrl: 'date.php',
+	});
+});
+<?php echo htmlentities(ob_get_clean()); ?>
+</pre>
+		</td>
+	</tr>
+	<tr>
+		<td class="demonstration"><div id="clock6" class="clock"><?php echo date('Y/m/d H:i:s'); ?></div></td>
+		<td class="summary">Default behaviour with initial server formatted time and AJAX updates (also formatted time)</td>
+		<td class="explanation">Formatted date from server:<br/><code><?php echo date('Y/m/d H:i:s'); ?></code></td>
+		<td class="explanation">Once per minute from <code>date.php</code> (returns a timestamp from server)</td>
+		<td class="explanation">None</td>
+		<td class="javascript">
+<pre>
+<?php ob_start(); ?>
+$(function() {
+	$('#clock6').clock({
+		ajaxInterval: 60000,
+		ajaxUrl: 'date2.php'
+	});
+});
+<?php echo htmlentities(ob_get_clean()); ?>
+</pre>
+		</td>
+	</tr>
+	</tbody>
+</table>
+
+<script type="text/javascript" src="jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="../jquery.clock/jquery.clock.js"></script>
+<script type="text/javascript">
+	$(function() {
+		$('#clock0').clock();
+		$('#clock1').clock();
+		$('#clock2').clock({
+			outputFormat: '%i minutes and %s seconds past %g%a on %l the %d%S of %F in the year %Y'
+		});
+		$('#clock3').clock({
+			outputFormat: '<span class="date-part">%d-%m-%Y</span> <span class="time-part">%H:%i:%s</span>'
+		});
+		$('#clock4').clock();
+		$('#clock5').clock({
+			ajaxInterval: 60000,
+			ajaxUrl: 'date.php'
+		});
+		$('#clock6').clock({
+			ajaxInterval: 60000,
+			ajaxUrl: 'date2.php'
+		});
+
+		$('td.javascript').each(function() {
+			var $popup = $('<div></div>').addClass('popup').html($(this).html()).hide().appendTo($(document.body));
+			$(this).html('Display').addClass('display-hover').hover(function(evt) {
+				console.log(evt);
+				$popup.css({ top: evt.pageY + 'px', left: evt.pageX - $popup.width() + 'px' }).show();
+			}, function() {
+				$popup.hide();
+			});
+		});
+	});
+</script>
+</body>
+</html>
---
+++ jquery.clock/jquery.clock.js
@@ -0,0 +1,158 @@
+/**
+ * jQuery Clock plugin
+ * (c) Leftclick.com.au
+ * Open source license, yahta yahta
+ * Requires jquery and date.js
+ */
+
+(function($) {
+
+	$.clock = {
+		version: '0.1',
+		defaults: {
+			updateInterval: 100,
+			ajaxInterval: false,
+			ajaxUrl: false,
+			outputFormat: '%Y-%m-%d %H:%i:%s',
+			dayNames: {
+				abbr: [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ],
+				full: [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ]
+			},
+			monthNames: {
+				abbr: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
+				full: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
+			},
+			ordinalSuffixes: {
+				default: 'th',
+				overrides: {
+					'1': 'st',
+					'2': 'nd',
+					'3': 'rd',
+					'21': 'st',
+					'22': 'nd',
+					'23': 'rd',
+					'31': 'st'
+				}
+			},
+			aliases: [
+				{
+					regex: /%r/,
+					replacement: '%D, %d %M %Y %H:%i:%s %O'
+				}
+			]
+		}
+	};
+
+	$.fn.clock = function(o) {
+		var self = this,
+			options = $.extend(true, {}, $.clock.defaults, o),
+			// Used to store the difference between local time and server time, to simplify calculations
+			serverOffset = 0,
+			// Parse the given text as a date object
+			parseDate = function(text) {
+				return (text.length === 0) ? Date.now() : (text.match(/^\d+$/) ? new Date(parseInt(text * 1000, 10)) : new Date(Date.parse(text)));
+			},
+			// Date formatting function.
+			formatDate = function(date, format) {
+				// If no format specified, or format is empty, just return the default textual representation
+				if (!format || format.toString().length === 0) {
+					return date.toString();
+				}
+				// Check for certain specific identifiers in the format
+				if (format === 'default') {
+					return date.toString();
+				}
+				if (format === 'locale') {
+					return date.toLocaleString();
+				}
+				// Utility formatting functions
+				var formatNumber = function(number, pad) {
+						var s = number.toString();
+						return pad ? ((s.length === 1) ? "0" + s : s) : s;
+					},
+					formatDayName = function(date, abbr) {
+						return options.dayNames[abbr ? 'abbr' : 'full'][date.getDay()];
+					},
+					formatDaySuffix = function(date) {
+						var d = date.getDate().toString();
+						return (options.ordinalSuffixes.overrides[d]) ? options.ordinalSuffixes.overrides[d] : options.ordinalSuffixes.default;
+					},
+					formatMonthName = function(date, abbr) {
+						return options.monthNames[abbr ? 'abbr' : 'full'][date.getMonth() - 1];
+					},
+					formatAmPm = function(date) {
+						return date.getHours() < 12 ? 'AM' : 'PM';
+					};
+				// Expand full format specifiers into their equivalent components
+				$.each(options.aliases, function(alias) {
+					format = format.replace(alias.regex, alias.replacement);
+				});
+				// Expand component format specifiers
+				return format.replace(/%\w/g, function(token) {
+					token = token.substring(1);
+					switch (token) {
+						case 'd': return formatNumber(date.getDate(), true);
+						case 'D': return formatDayName(date, true);
+						case 'j': return formatNumber(date.getDate(), false);
+						case 'l': return formatDayName(date, false);
+						//case 'N': return date.getDay() + 1; // Disabled because other correlating methods are not done
+						case 'S': return formatDaySuffix(date);
+						case 'w': return date.getDay();
+						//case 'z': return date.getDayOfYear(); // TODO
+						//case 'W': return date.getWeekOfYear(); // TODO
+						case 'F': return formatMonthName(date, false);
+						case 'm': return formatNumber(date.getMonth() + 1, true);
+						case 'M': return formatMonthName(date, true);
+						case 'n': return formatNumber(date.getMonth() + 1, false);
+						//case 't': return date.getDaysInMonth(); // TODO
+						//case 'L': return date.isLeapYear() ? '1' : '0'; // TODO
+						//case 'o': return date.getYear(); // Disabled because other correlating methods are not done
+						case 'Y': return date.getFullYear();
+						case 'y': return date.getFullYear().toString().substring(2,4);
+						case 'a': return formatAmPm(date).toLowerCase();
+						case 'A': return formatAmPm(date).toUpperCase();
+						//case 'B': // wtf?!
+						case 'g': return formatNumber(date.getHours() % 12, false);
+						case 'G': return formatNumber(date.getHours(), false);
+						case 'h': return formatNumber(date.getHours() % 12, true);
+						case 'H': return formatNumber(date.getHours(), true);
+						case 'i': return formatNumber(date.getMinutes(), true);
+						case 's': return formatNumber(date.getSeconds(), true);
+						//case 'u': return formatNumber(date.getMicroseconds(), true); // TODO
+						//case 'e': // TODO
+						//case 'I': // TODO
+						case 'O': return date.getTimezoneOffset()
+						//case 'P': // TODO
+						//case 'T': // TODO
+						//case 'Z': // TODO
+						default: return token;
+					}
+				});
+			},
+			// Utility functions
+			calculateServerOffset = function(serverDate) {
+				var localDate = Date.now();
+				return localDate - serverDate.getTime();
+			},
+			updateView = function() {
+				var d = new Date();
+				d.setTime(Date.now() + serverOffset);
+				self.html(formatDate(d, options.outputFormat));
+			},
+			updateModel = function() {
+				$.get(options.ajaxUrl, null, function(timestamp) {
+					serverOffset = calculateServerOffset(parseDate(timestamp.trim()));
+					updateView();
+				});
+			};
+		// Bootstrap
+		serverOffset = (self.text().trim().length > 0) ? calculateServerOffset(parseDate(self.text().trim())) : 0;
+		updateView();
+		if (options.updateInterval > 0) {
+			setInterval(updateView, options.updateInterval);
+		}
+		if (options.ajaxInterval > 0 && options.ajaxUrl) {
+			setInterval(updateModel, options.ajaxInterval);
+		}
+	}
+}(jQuery));
